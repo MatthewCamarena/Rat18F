da48876858a439ce6ba10e78cce14ec31ec10944
MatthewCamarena <camarena.matthew@csu.fullerton.edu>
2018-11-11 01:16:54 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,25 @@
+//
+//  main.cpp
+//  lexican
+//
+//  Created by Matthew Camarena on 9/23/18.
+//  Copyright © 2018 Matthew Camarena. All rights reserved.
+//
+
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <string>
 #include <cassert>
 #include "LexiconReader.h"
+#include "SyntaxAnalyzer.h"
 using namespace std;
 
 int main(int argc, const char * argv[]) {
     string fileName = "testLex.txt";
-    LexiconReader LR;
-    LR.buildLexiconRecordfromDatafile(fileName);
+    //LexiconReader LR;
+    //LR.buildLexiconRecordfromDatafile(fileName);
+    SyntaxAnalyzer SA;
+    SA.run(fileName);
     return 0;
 }
